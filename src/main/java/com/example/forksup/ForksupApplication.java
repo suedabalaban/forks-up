3d59d9cabfbdd26aff239d7068d1c27c80be7350
[suedabalaban] <suedabalaban@gmail.com>
2024-10-07 20:30:38 +0300

    first commit

---
+++ src/main/java/com/example/forksup/ForksupApplication.java
@@ -0,0 +1,13 @@
+package com.example.forksup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForksupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForksupApplication.class, args);
+	}
+
+}
